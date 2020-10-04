87e6c435e8b679b5634cca02973b61bf1614a9ce
zfima <zzfima@gmail.com>
2020-10-04 21:39:12 +0300

    events

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -6,15 +6,30 @@
         <div class="robot-name">{{selectedRobot.head.title}}
           <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
         </div> -->
-        <PartSelector :parts="availableParts.heads" position="top"/>
+        <PartSelector
+        :parts="availableParts.heads"
+        position="top"
+        @partSelected="part => selectedRobot.head=part"/>
     </div>
     <div class="middle-row">
-      <PartSelector :parts="availableParts.arms" position="left"/>
-      <PartSelector :parts="availableParts.torsos" position="center"/>
-      <PartSelector :parts="availableParts.arms" position="right"/>
+      <PartSelector
+      :parts="availableParts.arms"
+      position="left"
+      @partSelected="part => selectedRobot.leftArm=part"/>
+      <PartSelector
+      :parts="availableParts.torsos"
+      position="center"
+      @partSelected="part => selectedRobot.torsos=part"/>
+      <PartSelector
+      :parts="availableParts.arms"
+      position="right"
+      @partSelected="part => selectedRobot.rightArm=part"/>
     </div>
     <div class="bottom-row">
-      <PartSelector :parts="availableParts.bases" position="bottom"/>
+      <PartSelector
+      :parts="availableParts.bases"
+      position="bottom"
+      @partSelected="part => selectedRobot.base=part"/>
     </div>
     <div>
       <h1>Cart</h1>
